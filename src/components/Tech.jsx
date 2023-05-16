b66d7cfea42b683d7671525fa86682cf962e5bd7
Ruby pal <rubyp20001@gmail.com>
2023-05-16 22:46:45 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,13 +3,13 @@ import React from 'react'
       return(
         <div className="container m-4 w-28 p-5 text-center border  border-cyan-700 rounded-md shadow-lg shadow-cyan-700  hover:shadow-pink-500 transition-all ">
             
-                <a className='flex justify-center  items-center '>
+                <div className='flex justify-center  items-center '>
                 <button className='px-4'>
                     <div className='text-2xl font-extrabold' >{props.image}
 </div>
                     <p className='text-xl'>{props.tech}</p>
                     </button>
-                </a>
+                </div>
                 
             </div>
         
