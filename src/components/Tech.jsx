6c1ea9ba668ba8a5f5f7328f16adfc6e279601e7
Ruby pal <rubyp20001@gmail.com>
2023-05-16 22:40:48 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,7 +3,7 @@ import React from 'react'
       return(
         <div className="container m-4 w-28 p-5 text-center border  border-cyan-700 rounded-md shadow-lg shadow-cyan-700  hover:shadow-pink-500 transition-all ">
             
-                <a href="" className='flex justify-center  items-center '>
+                <a className='flex justify-center  items-center '>
                 <button className='px-4'>
                     <div className='text-2xl font-extrabold' >{props.image}
 </div>
